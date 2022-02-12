78b6a8d109cc355f2a99b2af92ad7503124b07a0
如初 <59353056+ruchuby@users.noreply.github.com>
2022-02-12 22:56:19 +0800

    游戏欢迎界面

--- C-Sharp学习/飞行棋/Program.cs
+++ C-Sharp学习/飞行棋/Program.cs
@@ -8,8 +8,26 @@ namespace 飞行棋
 {
     internal class Program
     {
+        public static void ShowGame()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("|********************|");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("|********************|");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("|*******飞行棋*******|");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("|********************|");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("|********************|");
+        }
+
+
         static void Main(string[] args)
         {
+            ShowGame(); // static方法只能调用static方法
         }
     }
 }
